c596bbede26dfeb9315af252d271e9813497b45e
azorfus <72391760+azorfus@users.noreply.github.com>
2023-08-13 20:24:58 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,115 +1,149 @@
+'''
+
+	Written by Azorfus
+	Github: @azorfus
+
+'''
+
 import socket
-import random
-from threading import Thread
+import threading
+import time
 from datetime import datetime
 import os
 
-# server's IP address
-# if the server is not on this machine, 
-# put the private (network) IP address (e.g 192.168.1.2)
-SERVER_HOST = "192.168.1.6"
-SERVER_PORT = 8080 # server's port
-separator_token = "<SEP>" # we will use this to separate the client name & message
-excode = False
+SERVER_HOST = "192.168.1.11"
+SERVER_PORT = 8080
+separator_token = "<SEP>"
 
-# initialize TCP socket
+fsport = 0
+
 s = socket.socket()
-print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
-# connect to the server
-s.connect((SERVER_HOST, SERVER_PORT))
-print("[+] Connected.")
-# prompt the client for a name
+
+# event = threading.Event()
+# event.set()
+
+try:
+	s.connect((SERVER_HOST, SERVER_PORT))
+	fsport = int(s.recv(256).decode().split("!@$:")[0])
+	print("File transport port: ", fsport)
+
+except ConnectionRefusedError:
+	print("Server is not available. Exiting...")
+	exit(1)
+
+print(f"[*] Connected to {SERVER_HOST}:{SERVER_PORT}.")
+
 name = input("Enter your name: ")
 
 def listen_for_messages():
 	while True:
-		message = s.recv(1024).decode()
-		if message == "server!exit!code":
-			excode = True
-		else:
-			print("\n" + message)
+		#if event.is_set():
+		msg_R = s.recv(1024).decode('utf-8', errors='ignore')
+		msg = msg_R.split("!@$:")[0]
+		if msg == "server!abort!code!abort":
+			s.close()
+			break
+		if msg != "file!download!incoming":
+			print("\n" + msg)
+		#else:
+		#	print("Paused Thread")
+		#	event.wait()
+		
 
 def file_transfer(file_name):
 	try:
 		file = open(file_name, "rb")
+		s.send("file!transfer!code".encode())
+		file_size = os.path.getsize(file_name)
+		rem = int(file_size % 4096)
+		quo = int((file_size - rem)/4096)
+		packet_info = str(quo) + ":@!$" + str(rem) + ":@!$" + file_name + ":@!$" + name + ":@!$"
+		send_packet = packet_info.ljust(256, "#")
+		s.send(send_packet.encode())
+
+		# packet transmission
+		s.sendall(file.read())
+
+		file.close()
+
 	except Exception as e:
-		print("Cannot open file.\n" + "-"*20 + f"\n{e}\n" + "-"*20  + "\n")
+		print(f"[!] Error: {e}")
 		return 0
 
-	s.send("file!transfer!code".encode())
-	file_size = os.path.getsize(file_name)
-	rem = int(file_size % 4096)
-	quo = int((file_size - rem)/4096)
-	packet_info = str(quo) + ":@!$" + str(rem) + ":@!$" + file_name + ":@!$" + name + ":@!$"
-	send_packet = packet_info.ljust(256, "#")
-	s.send(send_packet.encode())
-
-	# packet transmission
-	s.sendall(file.read())
-
-	file.close()
 	return 0
 
-def file_download(file_name):	
-	needed = file_name + ":@!"
-	needed_packet = needed.ljust(256, "#")
-	s.send(needed_packet.encode())
+def file_download(file_name):
+	print("\n[*] Establishing tunnel to server, this may take a moment...")
+	time.sleep(2)
+	fs = socket.socket()
+	try:
+		fs.connect((SERVER_HOST, fsport))
+		print(f"[*] Established tunnel to server on port: {fsport}")
+		packet_info_R = fs.recv(256)
+		packet_info = packet_info_R.decode('utf-8', errors='ignore')
+		packet_struct = packet_info.split(":@!")
 	
-	packet_info = s.recv(256).decode()
-	packet_struct = packet_info.split(":@!")
-
-	rem = int(packet_struct[1])
-	quo = int(packet_struct[0])
-	print(packet_struct[2], ", ", rem, ", ", quo)
-
-	file = open(packet_struct[2], "wb")
-
-	data = bytearray()
-	count = 0
-	packet = 0
-	while count <= quo:
-		print("counter: ", count)
-		packet = s.recv(4096)
-		print(packet)
-		data += packet
-		count += 1
+		rem = int(packet_struct[1])
+		quo = int(packet_struct[0])
+		req_file = packet_struct[2]
 	
-	file.write(data)
-	file.close()
-	print("file downloaded... ", f"file name: {packet_struct[2]}, size: {quo*4096 + rem} bytes.\n")
+		with open(req_file, "wb") as f:
+	
+			data = bytearray()
+			count = 0
+			packet = 0
+			while count <= quo:
+				if count == quo:
+					packet = fs.recv(rem)
+				else:
+					packet = fs.recv(4096)
+		
+				data += packet
+				count += 1
+		
+			f.write(data)
+			f.close()
+		print("[*] Closing file transfer tunnel...")
+		fs.close()
+		print("File downloaded...", f"file name: {packet_struct[2]}, size: {quo * 4096 + rem} bytes.\n")
+
+	except Exception as e:
+		print(f"[!] Could not establish file transfer tunnel.\nError: {e}")
+		fs.close()
+		return 0
 	return 0
 
-# make a thread that listens for messages to this client & print them
-t = Thread(target=listen_for_messages)
-# make the thread daemon so it ends whenever the main thread ends
-t.daemon = True
-# start the thread
-t.start()
+lisn = threading.Thread(target = listen_for_messages)
+lisn.daemon = True
+lisn.start()
 
 while True:
-	# input message we want to send to the server
 	date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-	to_send =  input()
-	# a way to exit the program
-	if to_send.lower() == 'quit' or excode:
-		quit_code = "client!exit!code"
-		s.send(quit_code.encode())
-		break
+	to_send = input()
 
-	elif to_send.strip() == "!upload file":
-		fileName = input("Enter file name: ")
-		file_transfer(fileName)
+	try:
+		if to_send.lower() == 'quit':
+			to_send = "client!exit!code!@:"
+			s.send(to_send.ljust(1024, "#").encode())
+			break
+	
+		elif to_send.strip() == "!upload file":
+			fileName = input("Enter file name: ")
+			file_transfer(fileName)
+	
+		elif to_send.strip() == "!download file":
+			fileName = input("Enter <user>__<file name>: ")
+			to_send = "file!download!request!@$:" + fileName + "!@:"
+			s.send(to_send.ljust(1024, "#").encode())
+			file_download(fileName)
+	
+		else:
+			to_send = f"[{date_now}] {name}{separator_token}{to_send}!@:"
+			s.send(to_send.ljust(1024, "#").encode())
+		
+	except Exception as e:
+		print(f"[!] Error: {e}")
 
-	elif to_send.strip() == "!download file":
-		fileName = input("Enter <user>__<file name>: ")
-		s.send("file!download!request".encode())
-		file_download(fileName)
-
-	else:
-		# add the datetime, name & the color of the sender
-		to_send = f"[{date_now}] {name}{separator_token}{to_send}"
-		# finally, send the message
-		s.send(to_send.encode())
-
-# close the socket
 s.close()
+
+
